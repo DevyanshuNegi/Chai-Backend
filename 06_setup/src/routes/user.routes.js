68a87810406c276706a44a9d4eeecf8ffd31ed11
DevyanshuNegi <devyanshunegi@gmail.com>
2024-04-28 22:00:26 +0530

    register api route done

--- 06_setup/src/routes/user.routes.js
+++ 06_setup/src/routes/user.routes.js
@@ -3,8 +3,10 @@ import { registerUser } from "../controllers/user.controller.js";
 
 const router = Router()
 
-router.route("/regiseter").post(registerUser) 
+router.route("/register").post(registerUser) 
 // register method is called
+// this is ther route that will be added after 
+// http://localhost:8000/api/v1/user
 
 export default router
  
